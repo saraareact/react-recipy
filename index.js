255566a0759b9768811389e5288e8343d2d2ee32
saraareact <110593630+saraareact@users.noreply.github.com>
2025-02-03 22:19:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const api = require('./src/routes/api')
+const sequelize=require('./src/dbConnects')
+const app = express();
+app.use(express.json());
+
+app.use(cors())
+app.use('/api', api)
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
